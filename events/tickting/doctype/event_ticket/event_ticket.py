fc39cfccc695bc8d6574cd48790c8f19a81c4484
awad <awd@hotmail.it>
2025-08-17 20:32:42 +0200

    feat: add QR code generation and new fields to Event Ticket DocType

--- events/tickting/doctype/event_ticket/event_ticket.py
+++ events/tickting/doctype/event_ticket/event_ticket.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, awad@hotmail.it and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -18,6 +18,44 @@ class EventTicket(Document):
 		attendee_name: DF.Data | None
 		booking: DF.Link | None
 		event: DF.Link
+		qr_code: DF.AttachImage | None
 		ticket_type: DF.Link
 	# end: auto-generated types
-	pass
+	
+
+	def after_insert(self):
+		self.generate_qr_code()
+
+	def generate_qr_code(self):
+		import io
+
+		import qrcode
+		event_type_title = frappe.db.get_value("Event Ticket Type", self.event, "title")
+		event_title = frappe.db.get_value("FE Events", self.event, "title")
+		img = qrcode.make(
+			f"Event Ticket: {self.name}\n"
+			f"Event: {event_title}\n"
+			f"Attendee: {self.attendee_name}\n"
+			f"Ticket Type: {event_type_title}"
+		)
+		qr_code = io.BytesIO()
+		img.save(qr_code, format='PNG')
+		hex_data =  qr_code.getvalue()
+		# attach the QR code image to the document in field qr_code
+		qr_code_file =  frappe.get_doc({
+			"doctype": "File",
+			"content": hex_data,
+			"attached_to_doctype": "Event Ticket",
+			"attached_to_name": self.name,
+			"is_private": 0,
+			"attached_to_field": "qr_code",
+			"file_name": f"ticket-qr-code-{self.name}.png"
+		}).save()
+
+		self.qr_code = qr_code_file.file_url
+		self.save()
+		frappe.db.commit()
+
+
+  
+  
