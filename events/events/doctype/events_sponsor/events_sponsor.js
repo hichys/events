d6f64d1210ea30dcb946ecf93462ee7e3e5f3d35
awad <awd@hotmail.it>
2025-08-17 03:48:02 +0200

    Revert "feat: Add Events Sponsor DocType and related files"
    This reverts commit 4074f518108919d35a598a2bd85c05c8139cd2fa.

--- events/events/doctype/events_sponsor/events_sponsor.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, awad@hotmail.it and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Events Sponsor", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- events/events/doctype/events_sponsor/events_sponsor.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, awad@hotmail.it and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class EventsSponsor(Document):
-	pass
