4972f993551df6e621c86718ffe42178cea9212d
awad <awd@hotmail.it>
2025-08-18 08:25:16 +0200

    feat: add Sponsorship and Proposal management features with new DocTypes and fields

--- events/events/doctype/sponsership_tier/sponsership_tier.py
+++ events/events/doctype/sponsership_tier/sponsership_tier.py
@@ -5,7 +5,7 @@
 from frappe.model.document import Document
 
 
-class EventsSponsor(Document):
+class SponsershipTier(Document):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
@@ -14,9 +14,7 @@ class EventsSponsor(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		company_name: DF.Data
-		event: DF.Link
-		logo: DF.AttachImage | None
-		tier: DF.Link
+		event: DF.Link | None
+		title: DF.Data
 	# end: auto-generated types
 	pass
--- events/proposals/doctype/sponsership_proposal/sponsership_proposal.py
+++ events/proposals/doctype/sponsership_proposal/sponsership_proposal.py
@@ -5,7 +5,7 @@
 from frappe.model.document import Document
 
 
-class EventsSponsor(Document):
+class SponsershipProposal(Document):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
@@ -14,9 +14,10 @@ class EventsSponsor(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		company_logo: DF.AttachImage | None
 		company_name: DF.Data
 		event: DF.Link
-		logo: DF.AttachImage | None
+		status: DF.Literal["Review Pending", "Accepted", "Rejected", "Withdrawn"]
 		tier: DF.Link
 	# end: auto-generated types
 	pass
